a279072081384d38c392976da86108d031a1b82b
Ch Bhumika <chintabhumika07@gmail.com>
2026-05-12 21:43:55 +0530

    add p1 file

---
+++ p1.py
@@ -0,0 +1,18 @@
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+c = input("enter an operation + , -, /, *: "
+"")
+
+
+if c == '+':
+    print( a + b)
+elif c == '-':
+    print(a-b)
+elif c == '*':
+    print(a*b)
+elif c == '/':
+    print(a/b)
+else: 
+    print("Enter a valid operation !")
+    
